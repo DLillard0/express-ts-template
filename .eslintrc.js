18b47668f7516f4871f43c04c4ace4ba644348d8
yuanjk@imyfone.cn <yuanjk@imyfone.cn>
2020-10-26 16:35:10 +0800

    2020年10月26日16:34:54 初始化

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+## express + ts 框架
+
+### 安装依赖
+```
+npm install
+```
+
+### 运行
+```
+npm run start
+```
+
+### 热更新运行
+```
+npm run dev
+```
+
+### 打包
+```
+npm run build
+// 将资源文件都放进dist文件夹
+npm run server
+```
